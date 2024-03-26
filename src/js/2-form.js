8b5f2dd6df06a9c39f57ed0a6a5716fb3aea19ad
RR <korvy85@gmail.com>
2024-03-26 06:37:38 +0200

    update form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,41 +1,28 @@
 const feedbackForm = document.querySelector('.feedback-form');
-const inputEL = document.querySelector('input[type="email"]');
-const textareaEL = document.querySelector('textarea[name="message"]');
+const inputEL = document.querySelector('input');
+const textareaEL = document.querySelector('textarea');
 
 
-console.log(feedbackForm);
+const STORAGE_KEY = 'feedback-form-state';
 
-console.log(inputEL);
+feedbackForm.addEventListener('submit', handleSubmit);
 
-console.log(textareaEL);
-
-
-
-
-
-
-// const LS_KEY = 'feedback-form-state';
-
-// feedbackForm.addEventListener('submit', handleSubmit);
-
-// localStorage.clear();
-
-// function handleSubmit(event) {
-//     event.preventDefault();
+function handleSubmit(event) {
+    event.preventDefault();
     
-//     const form = event.target.elements;
-//     const email = form.email.value;
-//     const message = form.message.value;
+    const form = event.target.elements;
+    const email = form.email.value;
+    const message = form.message.value;
     
-//     const localStorageValue = {};
-//     localStorageValue.email = email.trim();
-//     localStorageValue.message = message.trim();
+    const localStorageValue = {};
+    localStorageValue.email = email.trim();
+    localStorageValue.message = message.trim();
 
-//     console.log(localStorageValue);
+    console.log(localStorageValue);
 
-//     localStorage.setItem(LS_KEY, JSON.stringify(localStorageValue));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(localStorageValue));
 
-// };
+};
  
 
 
